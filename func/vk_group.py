8634e2541455ebebc3fab8fcb068064cda82c138
Poloz Alexey <polozhev@mail.ru>
2018-08-24 21:08:16 +0300

    Add keyboard. Basic functions added.

--- func/vk_group.py
+++ func/vk_group.py
@@ -1,64 +1,62 @@
 import time, requests, vk_api, json
 
-GROUP_ID = 151412216
-ALBUM_ID = 247265476
+GROUP_ID = 170498641
+# ALBUM_ID = 247265476
 
 with open('keys.json', 'r') as file:
-	s=json.loads(file.read())
+	s = json.loads(file.read())
 
-	vk=vk_api.VkApi(token=s['token'])
+	vk = vk_api.VkApi(token=s['token'])
 
-	vks=vk_api.VkApi(login=s['login'], password=s['password'])
-	vks.auth()
+	# vks=vk_api.VkApi(login=s['login'], password=s['password'])
+	# vks.auth()
 
-def send(user, cont, img=[]):
+def send(user, cont, img=[], keyboard=None):
 	for i in range(len(img)):
-		if img[i][0:5]!='photo':
-#Загружаем изображение на сервер
+		if img[i][0:5] != 'photo':
+			#Загружаем изображение на сервер
 			with open('re.jpg', 'wb') as file:
 				file.write(requests.get(img[i]).content)
 
-#Загружаем изображение в ВК
-			photo=vk_api.VkUpload(vks).photo('re.jpg', group_id=GROUP_ID, album_id=ALBUM_ID)[0]
-			img[i]='photo{}_{}'.format(photo['owner_id'], photo['id'])
+			#Загружаем изображение в ВК
+			# photo = vk_api.VkUpload(vks).photo('re.jpg', group_id=GROUP_ID, album_id=ALBUM_ID)[0]
+			# img[i] = 'photo{}_{}'.format(photo['owner_id'], photo['id'])
 
-	return vk.method('messages.send', {'user_id':user, 'message':cont, 'attachment':','.join(img)})
+	req = {
+		'user_id': user,
+		'message': cont,
+		'attachment': ','.join(img),
+	}
 
-read = lambda: [[i['user_id'], i['body'], i['attachments'] if 'attachments' in i else []] for i in vk.method('messages.get')['items'] if not i['read_state']][::-1]
+	if keyboard:
+		buttons = []
+		for j in keyboard:
+			line = []
+			for i in j:
+				line.append({
+					'action': {
+						'type': 'text',
+						'payload': '{\"button\": \"1\"}',
+						'label': i,
+					},
+					'color': 'default',
+				})
+			buttons.append(line)
 
-dial = lambda: [i['message']['user_id'] for i in vk.method('messages.getDialogs')['items']]
+		req['keyboard'] = json.dumps({
+			'one_time': False,
+			'buttons': buttons,
+		}, ensure_ascii=False)
 
-def info(user):
-	x=vk.method('users.get', {'user_ids': user, 'fields': 'verified, first_name, last_name, sex, bdate, photo_id, country, city, home_town, screen_name'})[0]
-	#, lang, phone, timezone
+	return vk.method('messages.send', req)
 
-#Форматируем дату
-	try:
-		bd = x.get('bdate').count('.')
-	except:
-		bd = 0
+def read():
+	messages = []
+	for i in vk.method('messages.getConversations')['items']:
+		for j in vk.method('messages.getHistory', {'user_id': i['conversation']['peer']['id']})['items']:
+			if not j['read_state'] and not j['out']:
+				messages.append((j['from_id'], j['body']))
+	return messages
 
-	if bd == 2:
-		bd = time.strftime('%Y%m%d', time.strptime(x['bdate'], '%d.%m.%Y'))
-	elif bd == 1:
-		bd = time.strftime('%m%d', time.strptime(x['bdate'], '%d.%m'))
-	else:
-		bd = 0
-
-	y = (
-		x.get('verified'),
-		x.get('first_name'),
-		x.get('last_name'),
-		x.get('sex'),
-		int(bd),
-		x.get('photo_id'),
-		(str(x.get('country')['id']) if x.get('country') else '0') + '/' + (str(x.get('city')['id']) if x.get('city') else '0'),
-		user,
-		x.get('screen_name'),
-		#x.get('lang'), #0
-		#x.get('phone'), #0
-		#x.get('timezone'), #3
-		#x.get('home_town'), #0
-	)
-
-	return tuple(i if i != None else 0 for i in y)
+def users():
+	return vk.method('groups.getMembers', {'group_id': 170498641})['items']
